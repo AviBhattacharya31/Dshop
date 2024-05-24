8cb99e64a08ac7ed7ac5fb400049c36391e714c2
AviBhattacharya31 <avibhattacharya31@gmail.com>
2024-05-24 17:25:45 +0530

    initial file

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+// import Navbar from './Navbar';
+// import About from './About';
+// import Home from './Home';
+// import Footer from './Footer';
+// import{ BrowserRouter, Routes, Route} from "react-router-dom";
+import Hooks from './Hooks';
+
+
+function App() {
+    return (
+    <>
+
+    <Hooks/>
+      {/* <BrowserRouter>
+       <Navbar/>
+        
+       <Routes>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+       </Routes>
+       <Footer/>
+      </BrowserRouter> */}
+     </>
+  )
+}
+
+export default App
